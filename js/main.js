f1d5a770693a9107c0eb19c54eea43effde9b7f3
Nabiha Tariq <nabiha.t101@gmail.com>
2026-07-17 15:34:28 -0400

    adding transition of buttons and pages

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main_content');
+
+function PageTransitions() {
+    //click through buttons
+    for (let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function() {
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].classList = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        })
+
+    }
+
+}
+
+PageTransitions();
