d0c8a7dc8a1fc43d7a7a3057a2a5598f5b3661f6
Nabiha Tariq <nabiha.t101@gmail.com>
2026-07-18 12:41:01 -0400

    added section interactivity

--- js/main.js
+++ js/main.js
@@ -9,11 +9,32 @@ function whenButtonClicked() {
     this.className += ' active-btn';
 }
 
+function whenSectionClicked(event) {
+    const id = event.target.dataset.id;
+    if (id){
+        sectBtns.forEach(btn => {
+            btn.classList.remove('active');
+        });
+    }
+    event.target.classList.add('active');
+
+    //hide other sections
+    sections.forEach(section => {
+        section.classList.remove('active');
+    })
+
+    const element = document.getElementById(id);
+    element.classList.add('active');
+}
+
 function PageTransitions() {
     //click through buttons
     for (let i = 0; i < sectBtn.length; i++){
         sectBtn[i].addEventListener('click', whenButtonClicked)
     }
+
+    //sections active class
+    allSections.addEventListener('click', whenSectionClicked)
 }
 
 PageTransitions();
